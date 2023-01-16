b72aa28008fe1848c34e64c0d1173376d7368574
theodesp <328805+theodesp@users.noreply.github.com>
2023-01-16 12:11:28 +0000

    34-SearchRangeArray

--- solutions/34-SearchRangeArray/SearchRangeArrayBinarySearch.js
+++ solutions/34-SearchRangeArray/SearchRangeArrayBinarySearch.js
@@ -3,6 +3,29 @@
  * @param {number} target
  * @return {number[]}
  */
-const searchRange = function(nums, target) {
-    
-};
+function searchRange(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  let start = -1;
+  let end = -1;
+
+  while (left <= right) {
+    let mid = left + Math.floor((right - left) / 2);
+    if (nums[mid] === target) {
+      start = mid;
+      end = mid;
+      while (start >= 0 && nums[start] === target) {
+        start--;
+      }
+      while (end <= nums.length - 1 && nums[end] === target) {
+        end++;
+      }
+      return [start + 1, end - 1];
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return [-1, -1];
+}
